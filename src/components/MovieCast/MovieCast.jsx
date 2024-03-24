514428fac7c72f8b8479268526136aae67da5a51
ElizabethCheb <2018sunnygirl2018@gmail.com>
2024-03-24 15:34:37 +0200

    second try

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -2,36 +2,53 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import css from './MovieCast.module.css';
+
+const API_KEY = 'ddfcfa905ee1776e8ee8ae68b48577c8';
+
 const MovieCast = () => {
   const { movieId } = useParams();
   const [cast, setCast] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchCast = async () => {
+      setLoading(true);
+      setError(null);
+      
       try {
         if (!movieId) return;
         const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits`, {
           params: {
-            api_key: 'ddfcfa905ee1776e8ee8ae68b48577c8', // Додайте ваш ключ API тут
+            api_key: API_KEY,
           },
         });
         setCast(response.data.cast);
       } catch (error) {
-        console.error('Error fetching cast:', error);
+        setError(error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchCast();
   }, [movieId]);
 
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
-    <div>
+    <div >
       <h2>Cast</h2>
-      <ul>
+      <ul className={css.actor}>
         {cast.map(actor => (
           <li key={actor.id}>
             <div>
-              <img src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} alt={actor.name} style={{ width: 350, height: 500 }} />
+              {actor.profile_path ? (
+                <img src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} alt={actor.name} style={{ width: 350, height: 500 }} />
+              ) : (
+                <div>No image available</div>
+              )}
               <span className={css.span}>{actor.name}</span>
             </div>
           </li>
@@ -40,4 +57,5 @@ const MovieCast = () => {
     </div>
   );
 };
+
 export default MovieCast;
